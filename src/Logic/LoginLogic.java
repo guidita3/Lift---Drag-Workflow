1911c646dd0b413c49ce406950613e03509131e0
s206689 <s206689@SOW7406F.soe.cranfield.ac.uk>
2014-03-11 18:30:47 +0000

    LoginLogic using UserDB

--- src/Logic/LoginLogic.java
+++ src/Logic/LoginLogic.java
@@ -4,14 +4,30 @@
  */
 package Logic;
 
+import Persistence.UserDB;
+
 /**
  *
  * @author s206689
  */
 public class LoginLogic {
     
-    public boolean checkUserAndPassword(String userName, String Passw){
-        return true;
+    public boolean checkUserAndPassword(String userName, String Passw) throws Exception{
+        try {
+            //Check if the username exists in the database
+            boolean exist = new UserDB().userNameExists(userName);
+            
+            //If it exists, check its password
+            //False is returned if it does not exsist or the password is not ok
+            if (exist)
+            {
+               compUser check = new UserDB().findPersonbyUserName(userName);
+               if ( Passw.equals(check.getPassword()) ) return true;
+                 else return false;      
+            }
+            else return false;
+
+        } catch (Exception e){throw e;} //Database error
     }
 
 }
