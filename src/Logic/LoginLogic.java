e85aea63025aa8b20136bc4e4dd644a48031f4e5
Marta <Marta@MM-PC>
2014-03-13 13:14:48 +0000

    LoginLogic with registration

--- src/Logic/LoginLogic.java
+++ src/Logic/LoginLogic.java
@@ -10,7 +10,6 @@ import Persistence.UserDB;
  */
 public class LoginLogic {
     
-    
     /**
      * Function that checks if the username and password given exist together
      * in the database, so the user could accede to the system.
@@ -21,9 +20,16 @@ public class LoginLogic {
      */
     public boolean checkUserAndPassword(String userName, String Passw) throws Exception{
         try {
+            System.out.printf("Username:\n");
+            System.out.printf(userName);
+            System.out.printf("\n");
+            System.out.printf("Password:\n");
+            System.out.printf(Passw);
+            System.out.printf("\n");
             //Check if the username exists in the database
             boolean exist = new UserDB().userNameExists(userName);
-            
+            if (exist) System.out.printf("exist");
+            else System.out.printf("it does not exist");
             //If it exists, check its password
             //False is returned if it does not exsist or the password is not ok
             if (exist)
@@ -36,5 +42,42 @@ public class LoginLogic {
 
         } catch (Exception e){throw e;} //Database error
     }
+    
+    
+    /**
+     * Function that registers a new username and password
+     * @param userName String containing the username to be checked
+     * @param Passw String containing the password to be checked
+     * @return {@code true} if the user has been registered and {@code false} if not
+     * @throws Exception
+     */    
+    public boolean RegisterUser(String userName, String Passw) throws Exception{
+        try {
+            System.out.printf("Username:\n");
+            System.out.printf(userName);
+            System.out.printf("\n");
+            System.out.printf("Password:\n");
+            System.out.printf(Passw);
+            System.out.printf("\n");
+            
+            //Check if the username exists in the database
+            //If it exists returns false because it cannot be created again
+            //if not, it calls the function createRegisteredUser to register it
+            boolean exist = new UserDB().userNameExists(userName);
+            if (exist)System.out.printf("ya existe\n");
+            else System.out.printf("puedo crear\n");
+            
+            if (exist) return false;
+            else 
+            {
+                new UserDB().createRegisteredUser(userName,Passw);
+                return true;
+            }
+            
+        } catch (Exception e){throw e;} //Database error
+    }
+    
+    
+    
 
 }
