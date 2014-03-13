14d0c0fc08322b89a293964c20c92f738da9ce6b
Margarida Pereira <margaridagarciapereira@gmail.com>
2014-03-13 16:50:29 +0000

    compUserTest.java added.

---
+++ test/Logic/compUserTest.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Logic;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Pereira
+ */
+public class compUserTest {
+    
+    public compUserTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getSalt method, of class compUser.
+     */
+    @Test
+    public void testGetSalt() {
+        System.out.println("getSalt");
+        compUser instance = new compUser();
+        String expResult = "";
+        String result = instance.getSalt();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getUsername method, of class compUser.
+     */
+    @Test
+    public void testGetUsername() {
+        System.out.println("getUsername");
+        compUser instance = new compUser();
+        String expResult = "";
+        String result = instance.getUsername();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getPassword method, of class compUser.
+     */
+    @Test
+    public void testGetPassword() {
+        System.out.println("getPassword");
+        compUser instance = new compUser();
+        String expResult = "";
+        String result = instance.getPassword();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
